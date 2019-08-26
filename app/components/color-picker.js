74ab8bf36444f55f9a56243b30a22ba3e65c5f0d
Lionel Rudaz <lionelrudaz@icloud.com>
2019-08-26 14:01:37 +0200

    Test of click event

--- app/components/color-picker.js
+++ app/components/color-picker.js
@@ -3,4 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['d-flex', 'justify-content-between'],
   selectedColor: '',
+  click() {
+    alert('click on the picker');
+  }
 });
