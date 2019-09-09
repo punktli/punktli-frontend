56eb9994bd721bfb0a6c0186904557ef87bb1e39
Lionel Rudaz <lionelrudaz@icloud.com>
2019-09-09 16:28:41 +0200

    Init an event from patient

--- app/routes/event/new/wizard.js
+++ app/routes/event/new/wizard.js
@@ -2,8 +2,23 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  model() {
-    return this.modelFor('event.new');
+  queryParams: {
+    patientId: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    const model = this.modelFor('event.new');
+
+    if (params.patientId) {
+      return this.store.findRecord('patient', params.patientId).then(function(patient) {
+        model.set('patient', patient);
+        return model;
+      });
+    } else {
+      return model;
+    }
+
   },
   setupController(controller, model) {
     this._super(controller, model);
