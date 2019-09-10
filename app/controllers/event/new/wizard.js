c188840830b0674e14d2c362a54338afce66bb82
Lionel Rudaz <lionelrudaz@icloud.com>
2019-09-10 17:11:06 +0200

    Save an event

--- app/controllers/event/new/wizard.js
+++ app/controllers/event/new/wizard.js
@@ -1,6 +1,17 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   queryParams: ['patientId'],
-  patientId: null
+  patientId: null,
+  isButtonDisabled: computed('model.patient', 'model.category', 'model.startTime', function() {
+    return !(this.model.patient && this.model.category && this.model.startTime);
+  }),
+  actions: {
+    saveEvent() {
+      this.model.save().then(() => {
+        this.transitionToRoute('calendar');
+      })
+    }
+  }
 });
