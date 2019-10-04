69b9b6335e7c5baea04766360fd022abe187bdd0
Lionel Rudaz <lionelrudaz@icloud.com>
2019-10-04 18:03:15 +0200

    Fix the name of the event when category is set first

--- app/controllers/event/new/patient.js
+++ app/controllers/event/new/patient.js
@@ -4,7 +4,12 @@ export default Controller.extend({
   actions: {
     selectPatient(patient) {
       this.model.set('patient', patient);
-      this.model.set('name', patient.fullName);
+      if (this.model.category.get('name')) {
+        this.get('model').set('name', patient.fullName + ' / ' + this.model.category.get('name'));
+      } else {
+        this.model.set('name', patient.fullName);
+      }
+
       this.transitionToRoute('event.new.wizard');
     }
   }
